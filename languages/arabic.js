35c8af9200a4dde1bb793f2abe48099bfc67c3af
Nima Sharifkazemi <nima.nsk@hotmail.com>
2026-06-21 09:16:21 +0330

    Tune Arabic language priority

--- languages/arabic.js
+++ languages/arabic.js
@@ -9,6 +9,7 @@
         name: 'Arabic',
         regex: /[\u0600-\u06FF\u0750-\u077F\u08A0-\u08FF\uFB50-\uFDFF\uFE70-\uFEFF]/,
         fontStack: 'Noto Naskh Arabic, Noto Sans Arabic, Segoe UI, Tahoma, Arial, sans-serif',
-        direction: 'rtl'
+        direction: 'rtl',
+        priority: 20
     });
 })();
